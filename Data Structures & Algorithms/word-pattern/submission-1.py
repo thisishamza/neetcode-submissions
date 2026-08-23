b4c5b448db3709a7f8a92322b137f5bf9a03a070
Muhammad Hamza <mh.muhammadhamza97@gmail.com>
2026-08-24 03:06:16 +0500

    Add: word-pattern - submission-1

---
+++ Data Structures & Algorithms/word-pattern/submission-1.py
@@ -0,0 +1,6 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s = s.split()
+        if len(pattern) != len(s):
+            return False
+        return len(set(pattern)) == len(set(s))
